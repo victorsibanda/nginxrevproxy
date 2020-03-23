86f9a5d9eacf28852f999d491e5dcac73faa34de
Victor Sibanda <victormsibanda1@gmail.com>
2020-03-23 14:44:13 +0000

    edits made to provisioning file to download nodejs

--- environment/app/provisions.sh
+++ environment/app/provisions.sh
@@ -5,6 +5,18 @@ sudo apt-get upgrade -y
 sudo apt-get install nginx -y
 sudo unlink /etc/nginx/sites-enabled/default
 
+sudo apt-get install git
+curl -sL https://deb.nodesource.com/setup_6.x | bash -
+
+sudo apt-get install -y nodejs
+sudo apt install build-essential
+
+
+curl -L https://npmjs.org/install.sh | sudo sh
+
+sudo npm install pm2@latest -g
+
+
  sudo mv /home/ubuntu/app/reverse-proxy.conf /etc/nginx/sites-available/
 
  sudo ln -s /etc/nginx/sites-available/reverse-proxy.conf /etc/nginx/sites-enabled/reverse-proxy.conf
@@ -13,12 +25,3 @@ sudo unlink /etc/nginx/sites-enabled/default
 
 
 sudo apt-get install git -y
-
-
-sudo apt-get install git
-sudo apt-get install nginx -y
-curl -sL https://deb.nodesource.com/setup_6.x | bash -
-apt-get install -y nodejs
-sudo apt install build-essential
-curl -L https://npmjs.org/install.sh | sudo sh
-sudo npm install pm2@latest -g
