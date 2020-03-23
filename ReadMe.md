0b27049d0c77baf541b60e2701c938c5c59035ee
Victor Sibanda <victormsibanda1@gmail.com>
2020-03-23 10:41:35 +0000

    initial commit

---
+++ ReadMe.md
@@ -0,0 +1,22 @@
+# Task 3 - Multi Vagrant Machine
+
+
+### In order to use this VM:
+`git clone git@github.com:victorsibanda/VagrantTask23.git`
+
+
+### To Run Use:
+
+`vagrant up`
+
+
+#### For app
+
+`vagrant ssh app`
+`cd /home/ubuntu/app`
+`sudo npm install`
+`sudo npm start`
+
+Go to  ---> https://development.local/
+
+#### For db
---
+++ environment/app/provisions.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install nginx -y
+sudo unlink /etc/nginx/sites-enabled/default
+
+ sudo mv /home/ubuntu/app/reverse-proxy.conf /etc/nginx/sites-available/
+
+ sudo ln -s /etc/nginx/sites-available/reverse-proxy.conf /etc/nginx/sites-enabled/reverse-proxy.conf
+
+ sudo service nginx restart
+
+
+sudo apt-get install git -y
+
+
+sudo apt-get install git
+sudo apt-get install nginx -y
+curl -sL https://deb.nodesource.com/setup_6.x | bash -
+apt-get install -y nodejs
+sudo apt install build-essential
+curl -L https://npmjs.org/install.sh | sudo sh
+sudo npm install pm2@latest -g
